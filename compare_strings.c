48640ba6680284d3bc3726a82251a6f1c3300bf8
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-08-07 11:31:53 +0100

    push

--- compare_strings.c
+++ compare_strings.c
@@ -1,4 +1,4 @@
-#include <stab.h>
+#include <stdio.h>
 /*
   3. Comparer deux chaînes sans strcmp
 Lire deux chaînes et indiquer si elles sont égales ou non.
@@ -12,7 +12,7 @@ Les chaînes sont identiques.
 */
 
 int main(){
-    int n=0;
+  int n = 0;
     char chaine[27];
     char chaine2[27];
 
@@ -20,20 +20,27 @@ int main(){
     getchar();
     scanf("%s", chaine2);
     getchar();
+    // On suppose que les chaînes sont de même longueur et ne dépassent pas 26 caractères
 
+    // Comparaison caractère par caractère
+    // On utilise une boucle pour comparer chaque caractère des deux chaînes
+    // jusqu'à ce qu'on trouve une différence ou qu'on atteigne la fin de la chaîne
 
-    for (int i = 0; i < ; i++)
+    for (int i = 0; i < 27; i++)
     {
-        /* code */
+        if (chaine[i] == '\0' && chaine2[i] == '\0') {
+            break;
+        }
+        if (chaine[i] != chaine2[i]) {
+            n = 1;
+            break;
+        }
     }
-    
 
-   
-    
-    
-    
-    printf("Chaîne concaténée  : %s", resulta);
-
-    return 0;
+    if (n == 0)
+        printf("Les chaînes sont identiques.\n");
+    else
+       printf("Les chaînes sont différentes.\n");
+       return 0;
     
 }
