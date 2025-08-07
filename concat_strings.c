5e3254f169f22d5c1f271ba6e70f6b8c5268fb22
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-08-07 12:16:19 +0100

    mod

--- concat_strings.c
+++ concat_strings.c
@@ -16,35 +16,16 @@ int main(){
     int n=0;
     char chaine[27];
     char chaine2[27];
-    char resulta[55];
 
     scanf("%s", chaine);
     getchar();
     scanf("%s", chaine2);
     getchar();
 
-    int i = 0;
-   
-    while (chaine[i] != '\0')
-    {
-        resulta[i]= chaine[i];
-        printf("%c", resulta[i]);
-        resulta[i+1]='\0';
-        i++;
-    }
 
-    int j=0;
-     while (chaine2[j] != '\0')
-    {
-        resulta[i]=chaine2[j];
-        printf("%c", resulta[i]);
-        resulta[i+1]='\0';
-        i++;
-        j++;
-    }
-    
-    
-    printf("Chaîne concaténée  : %s", resulta);
+    printf("%s%s", chaine, chaine2);
+
+   
 
     return 0;
     
